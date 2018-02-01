26359299da73acc17fcb60ee0e993aeedda62dfa
SYtor <shulyakyarik@gmail.com>
2018-02-01 15:40:41 +0200

    wtf viewport?

---
+++ core/src/ua/syt0r/CustomLevel.java
@@ -0,0 +1,11 @@
+package ua.syt0r;
+
+/**
+ * Created by Yaroslav on 2018/02/01.
+ */
+public interface CustomLevel {
+
+    void initialize();
+    void loop();
+
+}
---
+++ core/src/ua/syt0r/Stage1.java
@@ -0,0 +1,20 @@
+package ua.syt0r;
+
+/**
+ * Created by Yaroslav on 2018/02/01.
+ */
+public class Stage1 implements CustomLevel {
+
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+}
